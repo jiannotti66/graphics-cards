4cadbc3262b451fbac213abc57a08845648ceb20
jiannotti66 <121405260+jiannotti66@users.noreply.github.com>
2024-03-27 22:25:54 -0500

    finds all graphics cards available, prints the name, along the price and other specifications

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from bs4 import BeautifulSoup;
-import requests;
+from bs4 import BeautifulSoup
+import requests
 
 url = "https://www.newegg.com/GPUs-Video-Graphics-Cards/SubCategory/ID-48"
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
-print(doc.prettify)
 
-prices = doc.find_all(text="$")
-print(prices)
+items = doc.find_all("div", class_="item-container")
+for item in items:
+    name = item.find("a", class_="item-title").text.strip()
+    price = item.find("li", class_="price-current").find("strong").text.strip()
+    print("Name:", name)
+    print("Price:", price)
+    print()
