d92955de6ac6b6138e9a31d510379d657b875356
jiannotti66 <121405260+jiannotti66@users.noreply.github.com>
2024-03-27 22:29:14 -0500

    small commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+# for now we are working with newegg graphics cards but we can change the catalog we are looking for later.
 url = "https://www.newegg.com/GPUs-Video-Graphics-Cards/SubCategory/ID-48"
 
 result = requests.get(url)
@@ -11,5 +12,5 @@ for item in items:
     name = item.find("a", class_="item-title").text.strip()
     price = item.find("li", class_="price-current").find("strong").text.strip()
     print("Name:", name)
-    print("Price:", price)
+    print("Price: $" + price)
     print()
