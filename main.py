8438b50b51ccf67d3e0e2346b5297156402a4e9c
jiannotti66 <121405260+jiannotti66@users.noreply.github.com>
2024-04-23 14:23:01 -0500

    cleaned it up, made a method that can check cards within a maximum price range.

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
-# for now we are working with newegg graphics cards but we can change the catalog we are looking for later.
-url = "https://www.newegg.com/GPUs-Video-Graphics-Cards/SubCategory/ID-48"
+def findCards(maxPrice, url):
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "html.parser")
 
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
+    items = doc.find_all("div", class_="item-container")
+    numCards = 0
+    for item in items:
+        numCards = numCards + 1
+        name = item.find("a", class_="item-title").text.strip()
+        price_str = item.find("li", class_="price-current").find("strong").text.strip()
+        if price_str == "COMING SOON":
+            # Skip items labeled as "COMING SOON"
+            continue
+        price = int(price_str.replace(',', ''))
+        if price < maxPrice:
+            print("Name:", name)
+            print("Price: $" + price_str)
+            print()
+            
+    print("Number of cards available: " + str(numCards))
 
-items = doc.find_all("div", class_="item-container")
-numCards = 0
-for item in items:
-    numCards = numCards + 1
-    name = item.find("a", class_="item-title").text.strip()
-    price = item.find("li", class_="price-current").find("strong").text.strip()
-    print("Name:", name)
-    print("Price: $" + price)
-    print()
-print("Number of cards available: "+ str(numCards))
+findCards(500, "https://www.newegg.com/GPUs-Video-Graphics-Cards/SubCategory/ID-48")
